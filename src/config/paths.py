8ff3f356fb236498a8c4a1d61217652bd7e27620
LFM <318045638+lfmta@users.noreply.github.com>
2026-09-01 12:41:49 -0300

    fix: change dir paths

--- src/config/paths.py
+++ src/config/paths.py
@@ -8,8 +8,8 @@ class Paths:
 
     base: Path
     output_dir: Path = field(init=False)
-    proteomes_dir: Path = field(init=False)
-    genomes_dir: Path = field(init=False)
+    sequences_dir: Path = field(init=False)
+    redundant_seqs_dir: Path = field(init=False)
     paralogs_dir: Path = field(init=False)
     ortholog_groups_dir: Path = field(init=False)
 
@@ -17,7 +17,6 @@ class Paths:
     predicted_prots_db: Path = field(init=False)
     genomes_fasta: Path = field(init=False)
     proteomes_fasta: Path = field(init=False)
-    orfeomes_fasta: Path = field(init=False)
     ids_txt: Path = field(init=False)
 
     def __post_init__(self) -> None:
@@ -29,15 +28,15 @@ class Paths:
 
         object.__setattr__(self, "base", base)
         object.__setattr__(self, "output_dir", base / "output")
-        object.__setattr__(self, "proteomes_dir", base / "proteomes")
-        object.__setattr__(self, "genomes_dir", base / "genomes")
+        object.__setattr__(self, "sequences_dir", base / "sequences")
+        object.__setattr__(self, "redundant_seqs_dir", base / "redundant_seqs")
         object.__setattr__(self, "paralogs_dir", base / "paralogs")
         object.__setattr__(self, "ortholog_groups_dir", base / "ortholog_groups")
+
         object.__setattr__(self, "annotated_prots_db", base / "gb_unique_prots.fasta")
         object.__setattr__(
             self, "predicted_prots_db", base / "predicted_unique_prots.fasta"
         )
         object.__setattr__(self, "genomes_fasta", base / "genomes.fasta")
         object.__setattr__(self, "proteomes_fasta", base / "proteomes.fasta")
-        object.__setattr__(self, "orfeomes_fasta", base / "orfeomes.fasta")
         object.__setattr__(self, "ids_txt", base / "ids.txt")
