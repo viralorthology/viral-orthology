cfcf8362ff4e57d88fbba91d99b4ee6b17cb6eb0
LFM <318045638+lfmta@users.noreply.github.com>
2026-09-04 16:14:56 -0300

    docs: document pipeline stages

--- src/modules/pipeline/pipeline.py
+++ src/modules/pipeline/pipeline.py
@@ -3,3 +3,17 @@ from config.context import Context
 
 def run(ctx: Context) -> None:
     pass
+    ### VALIDATION ###
+
+    ### RUN ###
+
+    # preparation stage
+    # initial stage
+    # enrichment stage
+    # if redundant genomes
+    # # prepare for second round
+    # # enrichment stage
+    # final stage
+    # clean tmp files
+
+    ### MAKE REPORTS ###
