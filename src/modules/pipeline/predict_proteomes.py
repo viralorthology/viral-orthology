fa8d346743c30479141430b270dd0f0e78065734
LFM <318045638+lfmta@users.noreply.github.com>
2026-09-10 17:06:26 -0300

    feat: show message when predicting proteomes

--- src/modules/pipeline/predict_proteomes.py
+++ src/modules/pipeline/predict_proteomes.py
@@ -6,6 +6,8 @@ from models.fasta import Fasta
 
 def predict_proteomes(ctx: Context) -> None:
     """Predict proteomes from genome sequences and save the resulting proteomes."""
+    ctx.ui.show("Predicting proteomes...")
+
     genome_fastas = utils.get_fastas(ctx.paths.sequences_dir, ".genome")
     n_predicted_proteins = 0  # TODO if -download_seqs predicts proteome, change this
     for genome_fasta in ctx.ui.progress_bar(genome_fastas):
