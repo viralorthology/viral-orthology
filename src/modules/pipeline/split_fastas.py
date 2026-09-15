9e933936370bec73154d69125f3242544e137594
LFM <318045638+lfmta@users.noreply.github.com>
2026-09-15 14:01:51 -0300

    fix: do not split genome fastas

--- src/modules/pipeline/split_fastas.py
+++ src/modules/pipeline/split_fastas.py
@@ -5,14 +5,8 @@ from models.fasta import Fasta
 
 
 def split_fastas(ctx: Context) -> None:
-    """Split genomes and proteomes into one file per genome."""
+    """Split proteomes into one file per genome."""
     ctx.paths.sequences_dir.mkdir()
-
-    # make a fasta for every genome
-    genomes_fasta = Fasta(ctx.paths.genomes_fasta)
-    for genome_seq in genomes_fasta.seqs:
-        genome_fasta = Fasta(ctx.paths.sequences_dir / f"{genome_seq.id}.genome")
-        genome_fasta.add_seqs(genome_seq)
 
     # make a fasta for every proteome
     proteomes_fasta = Fasta(ctx.paths.proteomes_fasta)
