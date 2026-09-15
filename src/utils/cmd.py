9e3cbd3966859acf99d4283ea3c1321b6a3ee608
LFM <318045638+lfmta@users.noreply.github.com>
2026-09-15 15:21:43 -0300

    feat: add cwd parameter to run_cmd

--- src/utils/cmd.py
+++ src/utils/cmd.py
@@ -1,18 +1,23 @@
 import shlex
 import shutil
 import subprocess
+from pathlib import Path
 
 
-def run_cmd(cmd_str: str) -> str:
+def run_cmd(cmd_str: str, cwd: Path | None = None) -> str:
     """
     Run a command and return its standard output.
+
+    Args:
+        cmd_str: The command to run.
+        cwd: The working directory in which to run the command.
 
     Raises:
         subprocess.CalledProcessError: if the command exits with a non-zero status
         FileNotFoundError: if the command is not found
     """
     output = subprocess.run(
-        shlex.split(cmd_str), capture_output=True, text=True, check=True
+        shlex.split(cmd_str), capture_output=True, text=True, check=True, cwd=cwd
     )
     return output.stdout
 
