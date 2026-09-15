844160cf93fba7d7361c17afe9b2840ebbc473d4
LFM <318045638+lfmta@users.noreply.github.com>
2026-09-15 14:40:00 -0300

    fix: add predicted proteomes to predicted proteomes fasta

--- src/modules/pipeline/predict_proteomes.py
+++ src/modules/pipeline/predict_proteomes.py
@@ -7,17 +7,19 @@ def predict_proteomes(ctx: Context) -> None:
     """Predict proteomes from genome sequences and save the resulting proteomes."""
     ctx.ui.show("Predicting proteomes...")
 
+    predicted_proteomes_fasta = Fasta(ctx.paths.predicted_proteomes_fasta)
     genomes_fasta = Fasta(ctx.paths.genomes_fasta)
     for genome_seq in ctx.ui.progress_bar(
         genomes_fasta.seqs, total=genomes_fasta.n_seqs
     ):
-        proteome_fasta = Fasta(ctx.paths.sequences_dir / f"{genome_seq.id}.proteome")
+        proteome_fasta = Fasta(
+            ctx.paths.sequences_dir / f"{genome_seq.id}.proteome"
+        )  # TODO it may not exist
         predicted_proteins = predict_proteome(
             genome_seq,
             ctx.args.tool_args["orffinder"],
             ctx.runtime.predicted_proteins_count,
             proteome_fasta,
         )
-        predicted_fasta = Fasta(ctx.paths.sequences_dir / f"{genome_seq.id}.predicted")
-        predicted_fasta.add_seqs(*predicted_proteins)
+        predicted_proteomes_fasta.add_seqs(*predicted_proteins)
         ctx.runtime.predicted_proteins_count += len(predicted_proteins)
