943b3b87be76d00cbdfe84efbe9a01a623df0af5
LFM <318045638+lfmta@users.noreply.github.com>
2026-09-15 14:40:25 -0300

    feat: add Runtime attr

--- src/config/runtime.py
+++ src/config/runtime.py
@@ -4,5 +4,6 @@ from dataclasses import dataclass
 @dataclass
 class Runtime:
     redundant_genomes: bool = False
+    redundant_genome_ids: set[str] | None = None
     predicted_proteins_count: int = 0
     active_genomes: list[str] | None = None
