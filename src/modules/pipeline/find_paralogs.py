dad998962da6651904d60f8da85bd414eafcdb5b
LFM <318045638+lfmta@users.noreply.github.com>
2026-09-17 16:10:05 -0300

    refactor: remove exist_ok in paralogs_dir mkdir

--- src/modules/pipeline/find_paralogs.py
+++ src/modules/pipeline/find_paralogs.py
@@ -17,7 +17,7 @@ def find_paralogs(ctx: Context) -> None:
     """
     ctx.ui.show("Searching for paralogs...")
 
-    ctx.paths.paralogs_dir.mkdir(exist_ok=True)
+    ctx.paths.paralogs_dir.mkdir()
     proteomes = utils.get_fastas(ctx.paths.sequences_dir, ".proteome")
 
     for proteome in ctx.ui.progress_bar(proteomes):
